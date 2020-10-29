240260978fed6d77e72645200425bf9964e823ff
Yahor Krez <ykrez@gomel.iba.by>
2020-10-29 16:29:05 +0300

    All remarks have been resolved

---
+++ app.js
@@ -0,0 +1,13 @@
+function openNav() {
+    let main = document.querySelector(".content")
+    let nav = document.querySelector("nav")
+    nav.style.width = "250px"
+    main.style.marginLeft= "250px"
+}
+
+function closeNav() {
+    let main = document.querySelector(".content")
+    let nav = document.querySelector("nav")
+    nav.style.width = "0px"
+    main.style.marginLeft= "0px"
+}
