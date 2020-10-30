a520f5c6921c310cc54383152142f82f1737c636
Yahor Krez <ykrez@gomel.iba.by>
2020-10-30 20:50:13 +0300

    My site is responsive

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 function openNav() {
-    let main = document.querySelector(".main")
+    let main = document.querySelector(".short-bio")
     let nav = document.querySelector("nav")
-    nav.style.width = "250px"
-    main.style.marginLeft= "250px"
+    let navMenu = document.querySelector(".nav-menu")
+    let href = document.querySelector(".href-block")
+    nav.style.height = "280px"
+    navMenu.style.height = "280px"
 }
 
 function closeNav() {
-    let main = document.querySelector(".main")
+    let main = document.querySelector(".short-bio")
     let nav = document.querySelector("nav")
-    nav.style.width = "0px"
-    main.style.marginLeft= "0px"
+    let navMenu = document.querySelector(".nav-menu")
+    nav.style.height = "50px"
+    navMenu.style.height = "50px"
 }
