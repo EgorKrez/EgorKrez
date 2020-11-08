4b6f2484ef1b06c58593301797b186aba989893a
Yahor Krez <ykrez@gomel.iba.by>
2020-11-08 15:47:13 +0300

    Input tegs accept values

--- app.js
+++ app.js
@@ -15,5 +15,39 @@ function closeNav() {
     navMenu.style.height = "50px"
 }
 
+let inputName = document.querySelector(".input-name")
+let inputEmail = document.querySelector(".input-email")
+let inputSubject = document.querySelector(".input-subject")
+let inputMessage = document.querySelector(".input-message")
+let button = document.querySelector(".field-button")
+
+let info = {
+    name: "",
+    email: "",
+    subject: "",
+    message: ""
+}
+
+let arrayInfo = []
+
+button.onclick = () => {
+    info.name = inputName.value
+    info.email = inputEmail.value
+    info.subject = inputSubject.value
+    info.message = inputMessage.value
+    if (info.name != "" && info.email != "" && info.subject != "" && info.message != "") {
+    inputName.value = ""
+    inputEmail.value = ""
+    inputSubject.value = ""
+    inputMessage.value = ""
+    arrayInfo.push(info)
+    arrayInfo.forEach(function(item, i) {
+        console.log( (i + 1) + ") " 
+        + "name: " + item.name +  "; email: " + item.email + 
+        "; subject: " + item.subject +  "; message: " + item.message);
+      });
+    }
+    else alert(`Заполните все поля!\n`)
+}
 
 
